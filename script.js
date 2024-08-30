70f5f3afa5d90c146d57b9abb2c31c3034f6f29f
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-30 21:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function renderCart() {
   cartList.innerHTML = ""; // Clear previous list
   cart.forEach((item) => {
     const li = document.createElement("li");
-    li.innerHTML = `${item.name} - $${item.price}`;
+    li.innerHTML = `${item.name} - ${item.price}`;
     cartList.appendChild(li);
   });
 }
